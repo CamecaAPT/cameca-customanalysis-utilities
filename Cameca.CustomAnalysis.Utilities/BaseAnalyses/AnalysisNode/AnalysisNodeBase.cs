70550188425d2da34da4d352893ca8e6c8c3fc52
Jim Payne <jim.payne@ametek.com>
2022-05-24 17:29:46 -0500

    Add data state changed event handlers

--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisNode/AnalysisNodeBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisNode/AnalysisNodeBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using Cameca.CustomAnalysis.Interface;
 
@@ -42,7 +43,29 @@ public abstract class AnalysisNodeBase : CoreNodeBase<IAnalysisNodeBaseServices>
 			saveInterceptor.SavePreviewInterceptor = OnPreviewSave;
 		}
 		DataState = Services.DataStateProvider.Resolve(InstanceId);
+		if (DataState is not null)
+		{
+			DataState.PropertyChanged += DataStateOnPropertyChangedRouter;
+		}
 	}
+
+	private void DataStateOnPropertyChangedRouter(object? sender, PropertyChangedEventArgs e)
+	{
+		if (DataState is null) return;  // Should never occur if only handling DataState property
+		switch (e.PropertyName)
+		{
+			case nameof(INodeDataState.IsValid):
+				OnDataIsValidChanged(DataState.IsValid);
+				break;
+			case nameof(INodeDataState.IsErrorState):
+				OnDataErrorStateChanged(DataState.IsErrorState);
+				break;
+		}
+	}
+
+	protected virtual void OnDataIsValidChanged(bool isValid) { }
+
+	protected virtual void OnDataErrorStateChanged(bool isErrorState) { }
 
 	protected virtual void RequestDisplayViews()
 	{
@@ -61,5 +84,17 @@ public abstract class AnalysisNodeBase : CoreNodeBase<IAnalysisNodeBaseServices>
 
 	protected virtual bool MatchExistingViewPredicate(Type targetType, object viewModel)
 		=> viewModel.GetType() == targetType;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			if (DataState is not null)
+			{
+				DataState.PropertyChanged -= DataStateOnPropertyChangedRouter;
+			}
+		}
+		base.Dispose(disposing);
+	}
 }
 #nullable disable
--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/DataFilterNode/DataFilterNodeBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/DataFilterNode/DataFilterNodeBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Threading;
 using Cameca.CustomAnalysis.Interface;
 
@@ -20,12 +21,34 @@ public abstract class DataFilterNodeBase : CoreNodeBase<IDataFilterNodeBaseServi
 			saveInterceptor.SavePreviewInterceptor = OnPreviewSave;
 		}
 		DataState = Services.DataStateProvider.Resolve(InstanceId);
+		if (DataState is not null)
+		{
+			DataState.PropertyChanged += DataStateOnPropertyChangedRouter;
+		}
 		if (Services.DataFilterInterceptorProvider.Resolve(InstanceId) is { } dataFilterInterceptor)
 		{
 			dataFilterInterceptor.FilterDelegate = GetIndicesDelegate;
 			dataFilterInterceptor.IsInverted = IsInverted;
 		}
 	}
+
+	private void DataStateOnPropertyChangedRouter(object? sender, PropertyChangedEventArgs e)
+	{
+		if (DataState is null) return;  // Should never occur if only handling DataState property
+		switch (e.PropertyName)
+		{
+			case nameof(INodeDataState.IsValid):
+				OnDataIsValidChanged(DataState.IsValid);
+				break;
+			case nameof(INodeDataState.IsErrorState):
+				OnDataErrorStateChanged(DataState.IsErrorState);
+				break;
+		}
+	}
+
+	protected virtual void OnDataIsValidChanged(bool isValid) { }
+
+	protected virtual void OnDataErrorStateChanged(bool isErrorState) { }
 
 	protected virtual string? FilterCalculationMessage => null;
 
@@ -36,4 +59,16 @@ public abstract class DataFilterNodeBase : CoreNodeBase<IDataFilterNodeBaseServi
 	protected virtual byte[]? OnSave() => null;
 
 	protected virtual byte[]? OnPreviewSave() => OnSave();
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing)
+		{
+			if (DataState is not null)
+			{
+				DataState.PropertyChanged -= DataStateOnPropertyChangedRouter;
+			}
+		}
+		base.Dispose(disposing);
+	}
 }
