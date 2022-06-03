8f49349732fd047513d22218913f929b878a4154
Jim Payne <jim.payne@ametek.com>
2022-06-03 16:23:44 -0500

    Add basic edit name dialog window

---
+++ Cameca.CustomAnalysis.Utilities/Controls/EditNameDialogView.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Cameca.CustomAnalysis.Utilities.Controls;
+/// <summary>
+/// Interaction logic for NameDialogView.xaml
+/// </summary>
+internal partial class EditNameDialogView
+{
+	public EditNameDialogView()
+	{
+		InitializeComponent();
+	}
+}
--- Cameca.CustomAnalysis.Utilities/PrismIocExtensions.cs
+++ Cameca.CustomAnalysis.Utilities/PrismIocExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Ioc;
+﻿using Cameca.CustomAnalysis.Utilities.Controls;
+using Prism.Ioc;
 
 namespace Cameca.CustomAnalysis.Utilities;
 
@@ -11,5 +12,10 @@ public static class PrismIocExtensions
 		containerRegistry.Register<IAnalysisNodeBaseServices, AnalysisNodeBaseServices>();
 		containerRegistry.Register<IDataFilterNodeBaseServices, DataFilterNodeBaseServices>();
 		containerRegistry.Register<IAnalysisViewModelBaseServices, AnalysisViewModelBaseServices>();
+	}
+
+	public static void RegisterDialogs(this IContainerRegistry containerRegistry)
+	{
+		containerRegistry.RegisterDialog<EditNameDialogView, EditNameDialogViewModel>(EditNameDialog.EditNameDialogName);
 	}
 }
