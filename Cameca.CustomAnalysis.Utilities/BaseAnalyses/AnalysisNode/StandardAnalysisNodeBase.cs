a9ce746494cb1e6fe994fe79f43ab5272874cf6e
Jim Payne <jim.payne@ametek.com>
2022-06-03 16:33:38 -0500

    Add rename support to statndard base nodes

--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisNode/StandardAnalysisNodeBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisNode/StandardAnalysisNodeBase.cs
@@ -1,13 +1,53 @@
 ﻿using System;
 using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities.Controls;
+using Prism.Commands;
+using Prism.Services.Dialogs;
 
 namespace Cameca.CustomAnalysis.Utilities;
 
 public abstract class StandardAnalysisNodeBase : AnalysisNodeBase
 {
-	protected StandardAnalysisNodeBase(IAnalysisNodeBaseServices services)
+	private readonly INodeMenuFactoryProvider _nodeMenuFactoryProvider;
+	private readonly INodeInfoProvider _nodeInfoProvider;
+	private readonly IDialogService _dialogService;
+
+	protected StandardAnalysisNodeBase(
+		IAnalysisNodeBaseServices services,
+		INodeMenuFactoryProvider nodeMenuFactoryProvider,
+		INodeInfoProvider nodeInfoProvider,
+		IDialogService dialogService)
 		: base(services)
 	{
+		_nodeMenuFactoryProvider = nodeMenuFactoryProvider;
+		_nodeInfoProvider = nodeInfoProvider;
+		_dialogService = dialogService;
+	}
+
+	protected override void OnInstantiated(INodeInstantiatedEventArgs eventArgs)
+	{
+		base.OnInstantiated(eventArgs);
+		if (_nodeMenuFactoryProvider.Resolve(InstanceId) is { } nodeMenuFactory)
+		{
+			nodeMenuFactory.ContextMenuItems = new[]
+			{
+				new MenuAction("Rename", new DelegateCommand(RenameCommand)),
+			};
+		}
+	}
+
+	protected virtual void RenameCommand()
+	{
+		var renameParameters = new EditNameDialogParameters
+		{
+			Title = "Rename",
+			Name = _nodeInfoProvider.Resolve(InstanceId)?.Name,
+			Validate = x => !string.IsNullOrWhiteSpace(x),
+		};
+		if (_dialogService.TryShowEditNameDialog(out string newName, renameParameters))
+		{
+			Services.EventAggregator.PublishNodeRename(InstanceId, newName);
+		}
 	}
 
 	protected sealed override bool InstanceIdFilter(INodeTargetEvent targetEventArgs)
--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/DataFilterNode/StandardDataFilterNodeBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/DataFilterNode/StandardDataFilterNodeBase.cs
@@ -1,11 +1,51 @@
 ﻿using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities.Controls;
+using Prism.Commands;
+using Prism.Services.Dialogs;
 
 namespace Cameca.CustomAnalysis.Utilities;
 
 public abstract class StandardDataFilterNodeBase : DataFilterNodeBase
 {
-	protected StandardDataFilterNodeBase(IDataFilterNodeBaseServices services) : base(services)
+	private readonly INodeMenuFactoryProvider _nodeMenuFactoryProvider;
+	private readonly INodeInfoProvider _nodeInfoProvider;
+	private readonly IDialogService _dialogService;
+
+	protected StandardDataFilterNodeBase(IDataFilterNodeBaseServices services,
+		INodeMenuFactoryProvider nodeMenuFactoryProvider,
+		INodeInfoProvider nodeInfoProvider,
+		IDialogService dialogService)
+		: base(services)
 	{
+		_nodeMenuFactoryProvider = nodeMenuFactoryProvider;
+		_nodeInfoProvider = nodeInfoProvider;
+		_dialogService = dialogService;
+	}
+
+	protected override void OnInstantiated(INodeInstantiatedEventArgs eventArgs)
+	{
+		base.OnInstantiated(eventArgs);
+		if (_nodeMenuFactoryProvider.Resolve(InstanceId) is { } nodeMenuFactory)
+		{
+			nodeMenuFactory.ContextMenuItems = new[]
+			{
+				new MenuAction("Rename", new DelegateCommand(RenameCommand)),
+			};
+		}
+	}
+
+	protected virtual void RenameCommand()
+	{
+		var renameParameters = new EditNameDialogParameters
+		{
+			Title = "Rename",
+			Name = _nodeInfoProvider.Resolve(InstanceId)?.Name,
+			Validate = x => !string.IsNullOrWhiteSpace(x),
+		};
+		if (_dialogService.TryShowEditNameDialog(out string newName, renameParameters))
+		{
+			Services.EventAggregator.PublishNodeRename(InstanceId, newName);
+		}
 	}
 
 	protected sealed override bool InstanceIdFilter(INodeTargetEvent targetEventArgs)
