f4ccbbb51e94022ae04d0e2c0e8424d04bd4e1f5
Jim Payne <jim.payne@ametek.com>
2022-06-06 13:00:41 -0500

    Add legacy node creation prompt

--- Cameca.CustomAnalysis.Utilities/Legacy/AnalysisMenuFactoryBase.cs
+++ Cameca.CustomAnalysis.Utilities/Legacy/AnalysisMenuFactoryBase.cs
@@ -1,6 +1,8 @@
 ﻿using Cameca.CustomAnalysis.Interface;
+using Cameca.CustomAnalysis.Utilities.Controls;
 using Prism.Commands;
 using Prism.Events;
+using Prism.Services.Dialogs;
 
 namespace Cameca.CustomAnalysis.Utilities.Legacy;
 
@@ -10,19 +12,34 @@ public abstract class AnalysisMenuFactoryBase : IAnalysisMenuFactory
 	protected abstract string NodeUniqueId { get; }
 
 	protected readonly IEventAggregator EventAggregator;
+	protected readonly IDialogService DialogService;
 
-	protected AnalysisMenuFactoryBase(IEventAggregator eventAggregator)
+	protected AnalysisMenuFactoryBase(IEventAggregator eventAggregator, IDialogService dialogService)
 	{
 		EventAggregator = eventAggregator;
+		DialogService = dialogService;
 	}
 
 	public IMenuItem CreateMenuItem(IAnalysisMenuContext context)
 	{
 		return new MenuAction(
 			DisplayInfo.Title,
-			new DelegateCommand(()
-				=> EventAggregator.PublishCreateNode(NodeUniqueId, context.NodeId, DisplayInfo.Title, DisplayInfo.Icon)),
+			new DelegateCommand(TryCreateAnalysis),
 			DisplayInfo.Icon);
+
+		void TryCreateAnalysis()
+		{
+			var parameters = new EditNameDialogParameters
+			{
+				Title = "Create Analysis",
+				Name = DisplayInfo.Title,
+				Validate = x => !string.IsNullOrWhiteSpace(x),
+			};
+			if (DialogService.TryShowEditNameDialog(out string nodeName, parameters))
+			{
+				EventAggregator.PublishCreateNode(NodeUniqueId, context.NodeId, nodeName, DisplayInfo.Icon);
+			}
+		}
 	}
 
 	public abstract AnalysisMenuLocation Location { get; }
