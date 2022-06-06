93668c8b074e16b58aa7de7fd45991ae531a001d
Jim Payne <jim.payne@ametek.com>
2022-06-06 10:36:28 -0500

    Add legacy options configuration

--- Cameca.CustomAnalysis.Utilities/PrismIocExtensions.cs
+++ Cameca.CustomAnalysis.Utilities/PrismIocExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using Cameca.CustomAnalysis.Utilities.Controls;
 using Prism.Ioc;
 
@@ -8,9 +9,43 @@ public static class PrismIocExtensions
 {
 	public static void AddCustomAnalysisUtilities(this IContainerRegistry containerRegistry, Action<UtilitiesOptions>? options = null)
 	{
+		// Create options and apply delegated configuration
 		var optionsInstance = new UtilitiesOptions();
 		options?.Invoke(optionsInstance);
-		if (optionsInstance.UseBaseClasses || optionsInstance.UseStandardBaseClasses)
+
+		// Select features from configured options
+		bool registerCoreBaseClasses = false;
+		bool registerStandardBaseClasses = false;
+		bool registerDialogs = false;
+		bool registerLegacy = false;
+
+		if (optionsInstance.UseBaseClasses)
+		{
+			registerCoreBaseClasses = true;
+		}
+		if (optionsInstance.UseStandardBaseClasses)
+		{
+			registerCoreBaseClasses = true;
+			registerStandardBaseClasses = true;
+			registerDialogs = true;
+		}
+		if (optionsInstance.UseDialogs)
+		{
+			registerDialogs = true;
+		}
+#pragma warning disable CS0618
+		// Disable obsolete warning for implementation of obsolete features
+		if (optionsInstance.UseLegacy)
+#pragma warning restore CS0618
+		{
+			registerCoreBaseClasses = true;
+			registerStandardBaseClasses = true;
+			registerDialogs = true;
+			registerLegacy = true;
+		}
+
+		// Apply registrations of configured options
+		if (registerCoreBaseClasses)
 		{
 			containerRegistry.Register<ICoreServices, CoreServices>();
 			containerRegistry.Register<ICoreNodeServices, CoreNodeServices>();
@@ -18,14 +53,17 @@ public static class PrismIocExtensions
 			containerRegistry.Register<IDataFilterNodeBaseServices, DataFilterNodeBaseServices>();
 			containerRegistry.Register<IAnalysisViewModelBaseServices, AnalysisViewModelBaseServices>();
 		}
-		if (optionsInstance.UseStandardBaseClasses)
+		if (registerStandardBaseClasses)
 		{
 			containerRegistry.Register<IStandardAnalysisNodeBaseServices, StandardAnalysisNodeBaseServices>();
 			containerRegistry.Register<IStandardDataFilterNodeBaseServices, StandardDataFilterNodeBaseServices>();
 		}
-		if (optionsInstance.UseDialogs || optionsInstance.UseStandardBaseClasses)
+		if (registerDialogs)
 		{
 			containerRegistry.RegisterDialog<EditNameDialogView, EditNameDialogViewModel>(EditNameDialog.EditNameDialogName);
+		}
+		if (registerLegacy)
+		{
 		}
 	}
 
