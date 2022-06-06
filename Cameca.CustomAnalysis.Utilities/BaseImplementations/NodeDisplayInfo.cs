dc02fd0fb9971c80ce829081c98b51076815e7de
Jim Payne <jim.payne@ametek.com>
2022-06-06 12:47:14 -0500

    Add base implementations and extensions

---
+++ Cameca.CustomAnalysis.Utilities/BaseImplementations/NodeDisplayInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+using Cameca.CustomAnalysis.Interface;
+
+namespace Cameca.CustomAnalysis.Utilities;
+
+public class NodeDisplayInfo : INodeDisplayInfo
+{
+	public string Title { get; }
+
+	public ImageSource? Icon { get; }
+
+	public NodeDisplayInfo(string title, ImageSource? icon = null)
+	{
+		Title = title;
+		Icon = icon;
+	}
+}
