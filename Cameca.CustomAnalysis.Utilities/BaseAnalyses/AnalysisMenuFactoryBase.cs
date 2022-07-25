0c35ca3435a168dceec0f104335a56fb060e50d9
Jim Payne <jim.payne@ametek.com>
2022-07-24 21:14:00 -0500

    Add menu support for enabled and tooltop parameters

--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisMenuFactoryBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/AnalysisMenuFactoryBase.cs
@@ -6,10 +6,17 @@ namespace Cameca.CustomAnalysis.Utilities;
 
 public abstract class AnalysisMenuFactoryBase : IAnalysisMenuFactory
 {
+	protected readonly IEventAggregator EventAggregator;
+
 	protected abstract INodeDisplayInfo DisplayInfo { get; }
+
 	protected abstract string NodeUniqueId { get; }
 
-	protected readonly IEventAggregator EventAggregator;
+	public abstract AnalysisMenuLocation Location { get; }
+
+	public virtual string? ToolTip { get; } = null;
+
+	public virtual bool IsEnabled { get; } = true;
 
 	protected AnalysisMenuFactoryBase(IEventAggregator eventAggregator)
 	{
@@ -21,8 +28,8 @@ public abstract class AnalysisMenuFactoryBase : IAnalysisMenuFactory
 		return new MenuAction(
 			DisplayInfo.Title,
 			new DelegateCommand(() => EventAggregator.PublishCreateNode(NodeUniqueId, context.NodeId, DisplayInfo.Title, DisplayInfo.Icon)),
-			DisplayInfo.Icon);
+			DisplayInfo.Icon,
+			IsEnabled,
+			ToolTip);
 	}
-
-	public abstract AnalysisMenuLocation Location { get; }
 }
