413efbfa3bc8b7472782425959f5c8bbee4dc52a
Jim Payne <jim.payne@ametek.com>
2023-06-26 12:55:09 -0500

    Add helper for setting read-only deferred member
    RequiredSetOnce wraps a generic value and throws if either the
    wrapped value is accessed before set (required) or has multiple
    attempts to set the value (set once).
    
    This is useful for deferred initialization of values that conceptually
    should be read-only but need to be set after the constructor. A common
    case will be having the IoC container resolve a type in a factory
    class but then have the factory wrapper set a unique instance ID or
    other server.

---
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/RequiredSetOnce.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Cameca.CustomAnalysis.Utilities;
+
+internal class RequiredSetOnce<T>
+{
+	private T _value = default!;
+
+	public bool IsSet { get; private set; } = false;
+
+	public T Value
+	{
+		get
+		{
+			if (!IsSet) throw new InvalidOperationException("Value must be set before accessing");
+			return _value;
+		}
+		set
+		{
+			if (IsSet) throw new InvalidOperationException("Value may only be set once.");
+			_value = value;
+			IsSet = true;
+		}
+	}
+
+	public static implicit operator T(RequiredSetOnce<T> setOnce)
+	{
+		return setOnce.Value;
+	}
+
+	public void EnsureSet(T value)
+	{
+		if (!IsSet)
+		{
+			Value = value;
+		}
+	}
+}
