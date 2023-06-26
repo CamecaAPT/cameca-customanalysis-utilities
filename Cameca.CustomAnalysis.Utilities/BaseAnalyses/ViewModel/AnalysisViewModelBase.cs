ff206d30a9dde2c3f6eed38c7ac366b15fe5c91e
Jim Payne <jim.payne@ametek.com>
2023-06-26 12:56:18 -0500

    Set ID on view model base using RequiredSetOnce

--- Cameca.CustomAnalysis.Utilities/BaseAnalyses/ViewModel/AnalysisViewModelBase.cs
+++ Cameca.CustomAnalysis.Utilities/BaseAnalyses/ViewModel/AnalysisViewModelBase.cs
@@ -1,16 +1,19 @@
 ﻿using Cameca.CustomAnalysis.Interface;
+using System.Xml.Linq;
+using Cameca.CustomAnalysis.Utilities.BaseAnalyses;
 
 namespace Cameca.CustomAnalysis.Utilities;
 
 public abstract class AnalysisViewModelBase<TNode> : CoreViewModelBase<IAnalysisViewModelBaseServices> where TNode : class
 {
-	protected TNode? Node { get; private set; } = null;
+	private RequiredSetOnce<TNode> _node = new();
+	protected TNode Node => _node;
 
 	protected AnalysisViewModelBase(IAnalysisViewModelBaseServices services) : base(services) { }
 
 	internal override void OnCreatedCore(ViewModelCreatedEventArgs eventArgs)
 	{
-		Node = Services.InstanceProvider.Resolve(eventArgs.OwnerNodeId) as TNode;
+		_node.Value = (TNode)Services.InstanceProvider.Resolve(eventArgs.OwnerNodeId)!;
 		base.OnCreatedCore(eventArgs);
 	}
 }
