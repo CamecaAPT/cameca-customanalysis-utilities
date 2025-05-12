44ce9a2215e287982e2924a4fa4cbf89bd832088
Jim Payne <jim.payne@ametek.com>
2025-05-12 16:03:17 -0500

    Add missing render data factory extensions
    - ValuePoints
    - SelectionWidget
    - Series
    - Error (Bars)
    - Error (Dashes)

--- Cameca.CustomAnalysis.Utilities/ExtensionMethods/RenderDataFactoryExtensions.cs
+++ Cameca.CustomAnalysis.Utilities/ExtensionMethods/RenderDataFactoryExtensions.cs
@@ -27,7 +27,23 @@ public static class RenderDataFactoryExtensions
 		renderData.IsVisible = isVisible;
 		return renderData;
 	}
-	
+
+	public static IValuePointsRenderData CreateValuePoints(
+		this IRenderDataFactory renderDataFactory,
+		ReadOnlyMemory<Vector4> positionsWithValues,
+		IColorMap? colorMap = null,
+		string? name = null,
+		bool isVisible = true)
+	{
+		var renderData = renderDataFactory.CreateValuePoints();
+		renderData.PositionsWithValues = positionsWithValues;
+		renderData.ColorMap = colorMap;
+		if (name is not null)
+			renderData.Name = name;
+		renderData.IsVisible = isVisible;
+		return renderData;
+	}
+
 	public static ISpheresRenderData CreateSpheres(
 		this IRenderDataFactory renderDataFactory,
 		ReadOnlyMemory<Vector3> positions,
@@ -133,6 +149,110 @@ public static class RenderDataFactoryExtensions
 			renderData.Height = height.Value;
 		if (width.HasValue)
 			renderData.Width = width.Value;
+		if (name is not null)
+			renderData.Name = name;
+		renderData.IsVisible = isVisible;
+		return renderData;
+	}
+
+	public static ISelectionWidgetRenderData CreateSelectionWidget(
+		this IRenderDataFactory renderDataFactory,
+		Vector3 scaling,
+		SelectionWidgetController controllers = SelectionWidgetController.All,
+		Color color = default,
+		Vector3 translation = default,
+		float? controllersSize = null,
+		float? lineThickness = null,
+		string? name = null,
+		bool isVisible = true)
+	{
+		var renderData = renderDataFactory.CreateSelectionWidget();
+		renderData.Scaling = scaling;
+		renderData.Controllers = controllers;
+		renderData.ControllersColor = color;
+		renderData.LineColor = color;
+		renderData.Translation = translation;
+		if (controllersSize.HasValue)
+			renderData.ControllersSize = controllersSize.Value;
+		if (lineThickness.HasValue)
+			renderData.LineThickness = lineThickness.Value;
+		if (name is not null)
+			renderData.Name = name;
+		renderData.IsVisible = isVisible;
+		return renderData;
+	}
+
+	public static ISeriesRenderData CreateSeries(
+		this IRenderDataFactory renderDataFactory,
+		ReadOnlyMemory<Vector3> points,
+		Color color = default,
+		float? thickness = null,
+		MarkerShape markerShape = MarkerShape.Circle,
+		LineStyle lineStyle = LineStyle.Solid,
+		int? markerSize = null,
+		string? name = null,
+		bool isVisible = true)
+	{
+		var renderData = renderDataFactory.CreateSeries();
+		renderData.Positions = points;
+		renderData.Color = color;
+		renderData.MarkerColor = color;
+		if (thickness.HasValue)
+			renderData.Thickness = thickness.Value;
+		renderData.MarkerShape = markerShape;
+		renderData.LineStyle = lineStyle;
+		if (markerSize.HasValue)
+			renderData.MarkerSize = markerSize.Value;
+		if (name is not null)
+			renderData.Name = name;
+		renderData.IsVisible = isVisible;
+		return renderData;
+	}
+
+	public static IErrorRenderData CreateErrorBars(
+		this IRenderDataFactory renderDataFactory,
+		ReadOnlyMemory<ErrorDataPoint>? data,
+		Color color = default,
+		int? dashWidth = null,
+		float? alpha = null,
+		float? lineThickness = null,
+		string? name = null,
+		bool isVisible = true)
+	{
+		var renderData = renderDataFactory.CreateError();
+		renderData.DisplayType = ErrorDisplayType.Bars;
+		renderData.Data = data;
+		renderData.Color = color;
+		if (dashWidth.HasValue)
+			renderData.DashWidth = dashWidth.Value;
+		if (alpha.HasValue)
+			renderData.Alpha = alpha.Value;
+		if (lineThickness.HasValue)
+			renderData.LineThickness = lineThickness.Value;
+		if (name is not null)
+			renderData.Name = name;
+		renderData.IsVisible = isVisible;
+		return renderData;
+	}
+
+	public static IErrorRenderData CreateErrorStripes(
+		this IRenderDataFactory renderDataFactory,
+		ReadOnlyMemory<ErrorDataPoint>? data,
+		Color color = default,
+		float? alpha = null,
+		float? lineThickness = null,
+		string? name = null,
+		bool isVisible = true)
+	{
+		var renderData = renderDataFactory.CreateError();
+		renderData.DisplayType = ErrorDisplayType.Stripes;
+		renderData.Data = data;
+		renderData.Data = data;
+		renderData.Color = color;
+		if (alpha.HasValue)
+			renderData.Alpha = alpha.Value;
+		if (lineThickness.HasValue)
+			renderData.LineThickness = lineThickness.Value;
 		if (name is not null)
 			renderData.Name = name;
 		renderData.IsVisible = isVisible;
